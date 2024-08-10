aa09b34c18cc22def9a25273a5974ba547458a5a
Yoga Prata <yoga.putra@ist.id>
2024-08-10 20:53:03 +0700

    orang gila masukin tailwind base

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 document.getElementById('language-switch').addEventListener('change', function() {
   if (this.checked) {
-      document.getElementById('greeting').textContent = 'Basis Portofolio dan CV';
+      document.getElementById('greeting').textContent = 'Indut Jelek';
       document.getElementById('welcome-message').textContent = 'Selamat datang di situs kami.';
       document.getElementById('language-label').textContent = 'Bahasa Indonesia';
   } else {
-      document.getElementById('greeting').textContent = 'Base Portofolio and CV';
+      document.getElementById('greeting').textContent = 'Indut Bau';
       document.getElementById('welcome-message').textContent = 'Welcome to our website.';
       document.getElementById('language-label').textContent = 'English';
   }
